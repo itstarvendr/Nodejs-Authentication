ef4ac432e3b58df93bfe8a573f854ec3319b5a61
itstarvendr <136082470+itstarvendr@users.noreply.github.com>
2023-07-11 12:18:00 +0530

    Create authRoutes.js

---
+++ authRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/authController');
+
+// Registration route
+router.post('/register', authController.register);
+
+// Login route
+router.post('/login', authController.login);
+
+// Protected route
+router.get('/protected', authController.protected);
+
+module.exports = router;
